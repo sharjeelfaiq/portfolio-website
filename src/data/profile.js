63973b3b19a4c7d244a0a7b5d03093dfe070f975
Sharjeel Faiq <mrmalik610@gmail.com>
2025-04-24 13:14:14 +0500

    fix: correct spelling of 'Freelancer' in profile data

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,7 +1,7 @@
 const ProfileData = {
   img: require("../assets/Images/hero.png"),
   name: "Sharjeel Faiq",
-  professions: ["Web Developer", "Free Lancer", "Enthusiastic Dev"],
+  professions: ["Web Developer", "Freelancer", "Enthusiastic Dev"],
   info: ["Knack of building web applications using MERN stack.", "I enjoy designing tech websites."],
   resume: "https://drive.google.com/file/d/1sVAw8fCZLh0Ya7VgcTR014nUhoL6ly9u/view?usp=sharing",
 };
