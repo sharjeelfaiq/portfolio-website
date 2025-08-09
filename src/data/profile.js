a2e1b6fdce092fe4e7ce8b0dbda9618aaf3b7cff
Sharjeel Faiq <mrmalik610@gmail.com>
2025-08-09 16:22:18 +0500

    Update Resume URL

--- src/data/profile.js
+++ src/data/profile.js
@@ -3,7 +3,7 @@ const ProfileData = {
   name: "Sharjeel Faiq",
   professions: ["Web Developer", "Freelancer", "Enthusiastic Dev"],
   info: ["Knack of building web applications using MERN stack.", "I enjoy designing tech websites."],
-  resume: "https://drive.google.com/file/d/1CvJkC8zkQGIIAjilb8Gru9JK6is6clYk/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1G7HDI5MqlZCNzMDDHI_CQtJc2ZRWpeAS/view?usp=sharing",
 };
 
 export default ProfileData;
